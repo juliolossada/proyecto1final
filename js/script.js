842caeaa10f8a5fdb1a7bc8a037e4df3aaeef7cc
Julio <juliocesarlossadagmail.com>
2017-10-24 14:35:43 -0400

    Ya se ve en el html la api

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
-const apiURL ="https://api.pokemontcg.io/v1/cards"
+function buscador() {
+	const apiURL ="https://api.pokemontcg.io/v1/cards";
+	palabra = document.getElementById('buscador').value;
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', apiURL, true);
+	xhr.onload =() => {
+		var json = JSON.parse(xhr.responseText);
+		cartas = json.cards;
+		cartas.forEach(function(carta) {
+			if(palabra.toLowerCase() == carta.name.toLowerCase()){
+				respuesta = carta
+				document.getElementById("arr").innerHTML = respuesta.nationalPokedexNumber;
+
+			}
+			
+		});
+		
+	};
+	xhr.send();
+}
+
+
+
+
+
+
+
+
+
+/*const apiURL ="https://api.pokemontcg.io/v1/cards"
 	let cartas;
 	const xhr = new XMLHttpRequest();
 	xhr.open('GET', apiURL, true)
@@ -11,6 +40,6 @@ const apiURL ="https://api.pokemontcg.io/v1/cards"
     
 		});
 	};
-	xhr.send();
+	xhr.send();*/
 
 
