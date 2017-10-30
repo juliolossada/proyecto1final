a8235ed278c46c9dedd5dcd9f1f9e953a07c31e5
Julio <juliocesarlossadagmail.com>
2017-10-30 17:19:32 -0400

    TERMINADO

--- js/script.js
+++ js/script.js
@@ -1,20 +1,51 @@
+document.getElementById("button").addEventListener("click", function(event){
+    nombre = document.getElementById("nombre").value
+    correo = document.getElementById('email')
+	emailregex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if((nombre.length==0)||(nombre.length<4))
+    {	alert('El nombre ingresado es invalido')
+    	event.preventDefault()
+    }
+    
+	else if (emailregex.test(correo.value)) {
+
+	}
+	else {
+		alert('El email ingresado es invalido');
+
+		event.preventDefault();
+	}
+
+
+
+
+   
+});
+
+
+function keyCode(event) {
+    var x = event.keyCode;
+    if (x == 13) {
+        validaBlanco()
+    }
+};
+
 function validaBlanco() {
 	palabrita = document.getElementById('buscador').value;
-	if(palabrita.length == 0){
-		document.getElementById("nameheroe").innerHTML = "NO EXISTE EL SUPERHEROE"
+	if(palabrita.length == 0) 
+	{			document.getElementById("series").innerHTML = ""
+				document.getElementById("historia").innerHTML = ""
+				document.getElementById("nameheroe").innerHTML =""
+				document.getElementById("imagen").innerHTML = ""
+				document.getElementById("comics").innerHTML = ""
+				document.getElementById("series").innerHTML = ""	
+				document.getElementById("comics").innerHTML = "NO EXISTE EL SUPERHEROE"
 
 	}
 	else
 		buscador();
 	
-
-
 }
-
-
-
-
-
 
 
 
@@ -32,35 +63,57 @@ function buscador() {
 	palabra = document.getElementById('buscador').value;
 	var web = apiURL +palabra;
 	document.getElementById('buscador').value="";
+	console.log(palabra)
 	const xhr = new XMLHttpRequest();
 	xhr.open('GET', web, true);
-	xhr.onload =() => {
+
+	xhr.onprogress = function () {
+    document.getElementById("loading1").innerHTML = "<div class='loader'></div>"
+};
+	xhr.onload = 
+				document.getElementById("series").innerHTML = ""
+				document.getElementById("loading1").innerHTML = ""
+				document.getElementById("nameheroe").innerHTML =""
+				document.getElementById("historia").innerHTML = ""
+				document.getElementById("imagen").innerHTML = ""
+				document.getElementById("comics").innerHTML = ""
+				document.getElementById("series").innerHTML = ""
+				setTimeout(function () {
+		
 		var json = JSON.parse(xhr.responseText);
 		heroes = json.data.results;
-		console.log(palabra.length)
-		if(heroes.length==0)
-		{
-			document.getElementById("nameheroe").innerHTML = "No existe el superheroe";
-	
-		}
+			if(heroes.length == 0)
+			{	document.getElementById("loading1").innerHTML = ""
+				document.getElementById("series").innerHTML = ""
+				document.getElementById("comics").innerHTML = "NO EXISTE EL SUPERHEROE"
+			}
 			heroes.forEach(function(heroe) {
-			
+
 			if(palabra.toLowerCase() == heroe.name.toLowerCase()){
 				respuesta = heroe
+				document.getElementById("loading1").innerHTML = ""
 				document.getElementById("nameheroe").innerHTML = respuesta.name;
 				document.getElementById("historia").innerHTML = "<b class=tamanito >History</b><br>" +respuesta.description;
 				document.getElementById("imagen").innerHTML = "<img id='posi' src="+respuesta.thumbnail.path+"/portrait_xlarge.jpg>"
 				document.getElementById("comics").innerHTML = "<b class=tamanito>Number of comics</b><br>" + respuesta.comics.available;
-				document.getElementById("series").innerHTML = "<b class=tamanito>Number of series</b><br>" + respuesta.series.available;		
+				document.getElementById("series").innerHTML = "<b class=tamanito>Number of series</b><br>" + respuesta.series.available + "<br>";		
+			}
+			else {
+				document.getElementById("loading1").innerHTML = "NO EXISTE EL SUPERHEROE"
+
 			}
 			
 			
 			
 		});
 		
-	};
+	},2000);
 	xhr.send();
 }
+
+
+
+
 
 
 
